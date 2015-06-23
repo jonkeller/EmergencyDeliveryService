b4991fd4f3ed2047370d7863db4b1b43668e075a
Gene Chorba <Gchorba@gmail.com>
2015-06-23 18:00:53 -0400

    name change

---
+++ app/src/main/java/com/codetheroad/eds/MapsActivity.java
@@ -0,0 +1,12 @@
+package com.codetheroad.eds;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapsActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		// empty activity, tab opens google maps
+		super.onCreate(savedInstanceState);
+	}
+
+}
