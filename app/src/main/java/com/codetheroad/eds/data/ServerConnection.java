86d0524447e037fb0725c49c5422f9d7d137ebb1
jonkeller <kellejm2@gmail.com>
2015-06-24 03:41:22 -0400

    Add jars. Fix method that should be static

--- app/src/main/java/com/codetheroad/eds/data/ServerConnection.java
+++ app/src/main/java/com/codetheroad/eds/data/ServerConnection.java
@@ -4,7 +4,7 @@ package com.codetheroad.eds.data;
  * Created by Jon on 6/23/2015.
  */
 public abstract class ServerConnection {
-    public ServerConnection connect() {
+    public static ServerConnection connect() {
         return new MockServerConnection();
     }
 
