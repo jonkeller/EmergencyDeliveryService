055d6b63263ade6001d1362d3d93369f9a9f8f7f
Gene Chorba <Gchorba@gmail.com>
2015-06-23 21:37:28 -0400

    Homepage

--- app/src/main/java/com/codetheroad/eds/HomeActivity.java
+++ app/src/main/java/com/codetheroad/eds/HomeActivity.java
@@ -17,56 +17,56 @@ public class HomeActivity extends SharedPref {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_layout);
-		connectVariables();
-		populateFields();
+	//	connectVariables();
+		//populateFields();
 	}
 
-	private void populateFields() {
-		// retrieve data from shared preferences and populated edit text fields
-		homeUpdateArray[0]
-				.setText(String.format("%.2f", Double.parseDouble(getSavedData(
-						homeHourlyNoGas, HomeActivity.this))));
-		homeUpdateArray[1].setText(String.format("%.2f", Double
-				.parseDouble(getSavedData(homeHourlyWage, HomeActivity.this))));
-		homeUpdateArray[2].setText(String.format("%.2f", Double
-				.parseDouble(getSavedData(homeTotalTips, HomeActivity.this))));
-		homeUpdateArray[3].setText(String.format("%.2f", Double
-				.parseDouble(getSavedData(homeTotalHours, HomeActivity.this))));
-		homeUpdateArray[4]
-				.setText(String.format("%.2f", Double.parseDouble(getSavedData(
-						homeTotalIncome, HomeActivity.this))));
-	}
-
-	private void connectVariables() {
-		// Connect XML layout with Java
-		homeUpdateArray = new TextView[] { (TextView) findViewById(R.id.Home1),
-				(TextView) findViewById(R.id.Home2),
-				(TextView) findViewById(R.id.Home3),
-				(TextView) findViewById(R.id.Home4),
-				(TextView) findViewById(R.id.Home5) };
-		Button historyButton = (Button) findViewById(R.id.HistoryButton);
-		Button calcButton = (Button) findViewById(R.id.CalcButton);
-		historyButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// open history activity when history button is checked
-				Intent i = new Intent("com.davidtunnell.deliverypal.HISTORY");
-				startActivity(i);
-			}
-		});
-		calcButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// open default calculator when calculator button is checked
-				Intent calcIntent = new Intent();
-				calcIntent.setClassName("com.android.calculator2",
-						"com.android.calculator2.Calculator");
-				startActivity(calcIntent);
-			}
-		});
-	}
+//	private void populateFields() {
+//		// retrieve data from shared preferences and populated edit text fields
+//		homeUpdateArray[0]
+//				.setText(String.format("%.2f", Double.parseDouble(getSavedData(
+//						homeHourlyNoGas, HomeActivity.this))));
+//		homeUpdateArray[1].setText(String.format("%.2f", Double
+//				.parseDouble(getSavedData(homeHourlyWage, HomeActivity.this))));
+//		homeUpdateArray[2].setText(String.format("%.2f", Double
+//				.parseDouble(getSavedData(homeTotalTips, HomeActivity.this))));
+//		homeUpdateArray[3].setText(String.format("%.2f", Double
+//				.parseDouble(getSavedData(homeTotalHours, HomeActivity.this))));
+//		homeUpdateArray[4]
+//				.setText(String.format("%.2f", Double.parseDouble(getSavedData(
+//						homeTotalIncome, HomeActivity.this))));
+//	}
+//
+//	private void connectVariables() {
+//		// Connect XML layout with Java
+//		homeUpdateArray = new TextView[] { (TextView) findViewById(R.id.Home1),
+//				(TextView) findViewById(R.id.Home2),
+//				(TextView) findViewById(R.id.Home3),
+//				(TextView) findViewById(R.id.Home4),
+//				(TextView) findViewById(R.id.Home5) };
+//		Button historyButton = (Button) findViewById(R.id.HistoryButton);
+//		Button calcButton = (Button) findViewById(R.id.CalcButton);
+//		historyButton.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// open history activity when history button is checked
+//				Intent i = new Intent("com.davidtunnell.deliverypal.HISTORY");
+//				startActivity(i);
+//			}
+//		});
+//		calcButton.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				// open default calculator when calculator button is checked
+//				Intent calcIntent = new Intent();
+//				calcIntent.setClassName("com.android.calculator2",
+//						"com.android.calculator2.Calculator");
+//				startActivity(calcIntent);
+//			}
+//		});
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
